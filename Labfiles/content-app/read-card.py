5ea92cdee91994538d4192658cd6847b30ca76cc
dickinsonkeith <155771436+dickinsonkeith@users.noreply.github.com>
2026-02-13 13:32:13 -0700

    Update read-card.py

--- Labfiles/content-app/read-card.py
+++ Labfiles/content-app/read-card.py
@@ -1,3 +1,31 @@
+from dotenv import load_dotenv
+import os
+import sys
+import time
+import requests
+import json
+
+def main():
+    # Clear the console
+    os.system('cls' if os.name=='nt' else 'clear')
+    try:
+        # Get the business card
+        image_file = 'biz-card-1.png'
+        if len(sys.argv) > 1:
+            image_file = sys.argv[1]
+        # Get config settings
+        load_dotenv()
+        ai_svc_endpoint = os.getenv('ENDPOINT')
+        ai_svc_key = os.getenv('KEY')
+        analyzer = os.getenv('ANALYZER_NAME')
+
+        # Analyze the business card
+        analyze_card (image_file, analyzer, ai_svc_endpoint, ai_svc_key)
+        print("\n")
+
+    except Exception as ex:
+        print(ex)
+
 def analyze_card(image_file, analyzer, endpoint, key):
     # Everything below is now properly indented
     print(f"Analyzing {image_file}")
